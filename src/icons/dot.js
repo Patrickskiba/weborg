46ee2f7824a661427022687999f16515a20cb4e3
Patrick Skiba <pskiba@posteo.net>
2019-05-04 08:28:58 -0400

    svg dot

---
+++ src/icons/dot.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+export default ({ size = 1, color = "grey" }) => <svg  height={`${size}rem`} width={`${size}rem`}>
+    <circle cx='50%' cy='50%' r={`${size/2}rem`} fill={color} />
+</svg>
+
+
