b1d7cc9c6194cacd4dba8373aa9a1a0d556968d4
Patrick Skiba <pskiba@posteo.net>
2019-05-04 08:48:41 -0400

    branching for later

--- src/components/headline.js
+++ src/components/headline.js
@@ -2,17 +2,18 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import renderNode from '../utils/renderNode'
 import TextContent from './textContent'
+import Dot from '../icons/dot'
 
-const Stars = ({ level }) => <span>{'*'.repeat(level)} </span>
-    const State = ({ state }) => <span>{state} </span>
-    const ChildNodes = ({ children }) => children.length !== 0 &&  children.map((node, idx) => renderNode({ node, idx }))
+const Stars = () => <span><Dot size={.5} /></span>
+const State = ({ state }) => <span style={{marginLeft: '1rem'}} >{state} </span>
+const ChildNodes = ({ children }) => children.length !== 0 &&  children.map((node, idx) => renderNode({ node, idx }))
 const Elipses = ({ show }) => show ? <span>...</span> : <span></span>
 
     export default ({node}) => {
         const [showChildren, setShowChildren] = useState(true)
-        return <div>
-            <span onClick={() => setShowChildren(!showChildren)}>
-                <Stars level={node.level} />
+        return <div >
+            <span style={{position: 'relative'}} onClick={() => setShowChildren(!showChildren)}>
+                <Stars />
                 <State state={node.State} />
                 <TextContent content={node.content} />
             </span>
