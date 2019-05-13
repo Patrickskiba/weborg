7b7d80935416aeedd71fd4be77783b879f4d5928
Patrick Skiba <pskiba@posteo.net>
2019-05-13 06:01:48 -0400

    Using styled components

--- src/components/headline.js
+++ src/components/headline.js
@@ -4,21 +4,36 @@ import renderNode from '../utils/renderNode'
 import TextContent from './textContent'
 import Dot from '../icons/dot'
 
-const Stars = () => <span><Dot size={.5} /></span>
+import styled from 'styled-components'
+
+const Row = styled.div`
+    display: flex;
+    align-items: 'baseline';
+    flex-flow: column;
+`
+
+const RowItems = styled.div`
+    display: flex;
+    alignItems: 'baseline';
+`
+
+const Stars = () => <div><Dot size={.75}/></div>
 const State = ({ state }) => <span style={{marginLeft: '1rem'}} >{state} </span>
 const ChildNodes = ({ children }) => children.length !== 0 &&  children.map((node, idx) => renderNode({ node, idx }))
 const Elipses = ({ show }) => show ? <span>...</span> : <span></span>
 
-    export default ({node}) => {
-        const [showChildren, setShowChildren] = useState(true)
-        return <div >
-            <span style={{position: 'relative'}} onClick={() => setShowChildren(!showChildren)}>
-                <Stars />
+export default ({node}) => {
+    const [showChildren, setShowChildren] = useState(true)
+    return <Row>
+        <RowItems>
+            <Stars />
+            <div onClick={() => setShowChildren(!showChildren)}>
                 <State state={node.State} />
                 <TextContent content={node.content} />
-            </span>
-            <Elipses show={!showChildren} />
-            { showChildren && <ChildNodes children={node.children} /> }
-        </div> 
-    }
+            </div>
+        </RowItems>
+        <Elipses show={!showChildren} />
+        { showChildren && <ChildNodes children={node.children} /> }
+    </Row> 
+}
 
