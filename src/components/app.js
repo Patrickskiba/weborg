75dbf7cc22a894068e6c6c5da92bc81fd9c065f9
Patrick Skiba <pskiba@posteo.net>
2019-05-19 17:41:27 -0400

    Click on a file from dropbox and see it get parsed and rendered :)

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import parse from '../parser/index'
 import renderNode from '../utils/renderNode'
+import { Dropbox } from 'dropbox'
 import DropboxButton from './dropbox'
 import DropboxFiles from './dropbox-files'
 
+const initDropbox = () => {
+    const hashValue = window.location.hash
+    if(hashValue === "") return false
+    return new Dropbox({ accessToken: hashValue.substring(1).split('&')[0].replace('access_token=','')})
+}
 
-export default ({ text }) => <div>
-    <DropboxButton />
-    <DropboxFiles />
-    <hr />
-    { parse(text).map((node, idx) => renderNode({ node, idx })) }
-</div>
+export default () => {
+    const [text, setText] = useState('')
+    const client = initDropbox()
+    return <div>
+        <DropboxButton />
+        { client && <DropboxFiles client={client} setText={setText}/> }
+        <hr />
+        { parse(text).map((node, idx) => renderNode({ node, idx })) }
+    </div>
+}
 
 
--- src/components/dropbox-files.js
+++ src/components/dropbox-files.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import { Dropbox } from 'dropbox'
 
-const getFileList = () => {
-    const hashValue = window.location.hash
-    if(hashValue === "") return false
-    const client = new Dropbox({ accessToken: hashValue.substring(1).split('&')[0].replace('access_token=','')})
+const getFileList = (client, setOrgFiles) => {
     client.filesListFolder({ path: '', recursive: false, include_media_info: false, include_deleted: false, include_has_explicit_shared_members: false })
-        .then(x => console.log(x.entries.filter(entry => /org$/.test(entry.name) == true)))
+        .then(x => setOrgFiles(x.entries.filter(entry => /org$/.test(entry.name) == true)))
         .catch(x => console.error(x))
 }
 
-export default () => <div>
-    { getFileList() }
-</div>
+const getTempUrl = (client, path, setText) => {
+    const reader = new FileReader()
+    client.filesGetTemporaryLink({ path: path }).then(file => { 
+        const url = file.link
+        fetch(url).then(data => data.blob()).then(blob => reader.readAsText(blob))
+    }).catch(console.error)
+
+    reader.onload = () => setText(reader.result)
+}
+
+export default ({ client, setText }) => {
+    const [orgFiles, setOrgFiles] = useState([])
+    if( orgFiles.length === 0) { 
+        getFileList(client, setOrgFiles) 
+    }
+    return orgFiles.map((file, idx) => <div key={idx} onClick={() => getTempUrl(client, file.path_lower, setText)}> { file.path_lower } </div>)
+}
