105ee62dd2b461e1324014434a57e744e11de5da
Patrick Skiba <pskiba@posteo.net>
2019-05-24 06:15:53 -0400

    file explorer expands and contracts, highlights the currently selected file

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
 import parse from '../parser/index'
 import renderNode from '../utils/renderNode'
 import DropboxButton from './dropbox'
@@ -7,18 +8,47 @@ import DropboxFiles from './dropbox-files'
 import FileExplorer from './fileExplorer'
 import Database from '../utils/database'
 
+
+const Container = styled.div`
+    display: flex;
+`
+
+const SideBar = styled.div`
+    background: #dddddd;
+    height: 100vh;
+    width: ${props => props.sideBarVisible ? '10%' : '0px'};
+    display: ${props => props.sideBarVisible ? 'block' : 'none'};
+
+    @media (max-width: 400px) {
+        width: ${props => props.sideBarVisible ? '80%' : '0%'};
+        display: ${props => props.sideBarVisible ? 'block' : 'none'};
+    }
+`
+const MainArea = styled.div`
+    width: 90%;
+    @media (max-width: 400px) {
+        width: 20%;
+    }
+`
+
+
 export default () => {
     const [text, setText] = useState('')
     const [client, setClient] = useState(undefined)
+    const [sideBarVisible, setSideBarVisible] = useState(true)
 
     DropboxFiles(client, setClient)
 
-    return <div>
-        <DropboxButton />
-        <FileExplorer setText={setText}/>
-        <hr />
-        { parse(text).map((node, idx) => renderNode({ node, idx })) }
-    </div>
+    return <Container>
+        <SideBar sideBarVisible={sideBarVisible}>
+            <DropboxButton />
+            <FileExplorer setText={setText}/>
+        </SideBar>
+        <MainArea>
+            <button onClick={() => setSideBarVisible(!sideBarVisible)}>Hide/Show</button>
+            { parse(text).map((node, idx) => renderNode({ node, idx })) }
+        </MainArea>
+    </Container>
 }
 
 
--- src/components/fileExplorer.js
+++ src/components/fileExplorer.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import { get, keys } from 'idb-keyval'
+import styled from 'styled-components'
 
 const getText = (file, setText) => get(file).then(setText)
 
+const FileEntry = styled.div`
+    font-size: 14px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    color: #3c3c3c;
+    padding-left: 5px;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    background: ${props => props.highlighed ? "#b1b1b1" : "#dddddd"};
+`
 
 export default ({ setText }) => { 
     const [fileList, setFileList] = useState([])
+    const [selectedRow, setSelectedRow] = useState(null)
 
     keys().then(keys => setFileList(keys))
 
-    return fileList.map((file, idx) => <div key={idx} onClick={() => getText(file, setText)}>{file}</div>)
+    return fileList.map((file, idx) => { 
+        const highlighed = selectedRow == idx
+        return <FileEntry highlighed={highlighed} key={idx} onClick={() => {
+            getText(file, setText)
+            setSelectedRow(idx)
+        }}>{file}</FileEntry>
+    })
 }
