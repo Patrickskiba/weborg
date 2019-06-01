15d3e0f8a3b6eae4eaaea45983cadefcdf5b432a
Patrick Skiba <pskiba@posteo.net>
2019-06-01 15:28:52 -0400

    Using Dan Abramov inspired useFormInput custom hook

--- src/components/EditMode.js
+++ src/components/EditMode.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-export default ({ editNode, setEditNode }) => <div>
-    <button onClick={() => setEditNode()}>Clickith me</button>
-    {JSON.stringify(editNode)}
-    <div>{editNode.level}</div>
-    <div>{editNode.content.map(content => content.text)}</div>
-    <div>{editNode.children
-            .filter(x => x.type === 'section')
-            .map((x, idx) => <div key={idx}> {x.content.map(x => x.text)} </div>)}</div>
-    </div>
+const getHeadlineText = editNode => editNode.content.map(content => content.text)
+
+const getSectionText = editNode => editNode.children
+    .filter(x => x.type === 'section')
+    .map(x => x.content.map(x => x.text))
+    .join('\n')
+
+export default ({ editNode, setEditNode }) => {
+    const level = useFormInput(editNode.level)
+    const sectionText = useFormInput(getSectionText(editNode))
+    const headlineText = useFormInput(getHeadlineText(editNode))
+
+    return <div>
+        <button onClick={() => setEditNode()}>Clickith me</button>
+        {JSON.stringify(editNode)}
+        <input type="text" {...level} />
+        <input type="text" {...headlineText} />
+        <textarea {...sectionText} />
+        </div>
+}
+
+const useFormInput = initialValue => {
+    const [value, setVal] = useState(initialValue)
+
+    const onChange = e => setVal(e.target.value)
+
+    return { value, onChange }
+}
