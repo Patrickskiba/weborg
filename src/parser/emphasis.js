c1d824715f9763d7d8e303ccdadc17c1338d0266
Patrick Skiba <pskiba@posteo.net>
2019-08-12 06:12:39 -0400

    Organizing regexp with string literals

--- src/parser/emphasis.js
+++ src/parser/emphasis.js
@@ -1,15 +1,26 @@
+const boldStart = "\\*"
+const italicStart = "\\/"
+const underlineStart = "\\_"
+const strickthroughStart = "\\+"
+const http = "http:\\/\\/"
+const https = "https:\\/\\/"
+const timestampStart = "\\<\\d\\d\\d\\d\\-\\d\\d\\-\\d\\d"
+const eligibile = new RegExp(
+  `^.*(${boldStart}|${italicStart}|${underlineStart}|${strickthroughStart}|${http}|${https}|${timestampStart}).*`,
+)
+
 const identifyEmphasis = char => {
   switch (char) {
-    case '*':
-      return { type: 'bold', endMatch: /\*(\s|$)/ }
-    case '_':
-      return { type: 'underline', endMatch: /\_(\s|$)/ }
-    case '+':
-      return { type: 'strikethrough', endMatch: /\+(\s|$)/ }
-    case '/':
-      return { type: 'italic', endMatch: /\/(\s|$)/ }
-    case '<':
-      return { type: 'timestamp', endMatch: /\>(\s|$)/ }
+    case "*":
+      return {type: "bold", endMatch: /\*(\s|$)/}
+    case "_":
+      return {type: "underline", endMatch: /\_(\s|$)/}
+    case "+":
+      return {type: "strikethrough", endMatch: /\+(\s|$)/}
+    case "/":
+      return {type: "italic", endMatch: /\/(\s|$)/}
+    case "<":
+      return {type: "timestamp", endMatch: /\>(\s|$)/}
     default:
       return false
   }
@@ -17,10 +28,10 @@ const identifyEmphasis = char => {
 
 const identifyLink = protocol => {
   switch (protocol) {
-    case 'http://':
-      return { type: 'url' }
-    case 'https://':
-      return { type: 'url' }
+    case "http://":
+      return {type: "url"}
+    case "https://":
+      return {type: "url"}
     default:
       return false
   }
@@ -33,9 +44,12 @@ const reducer = (acc, val, idx, arr) => {
     const link = identifyLink(isLink[1])
     if (link) {
       const previousType = acc[acc.length - 1].type
-      acc.push({ type: link.type, text: [val] })
-      acc.push({ type: previousType, text: [] })
+      acc.push({type: link.type, text: [val]})
+      acc.push({type: previousType, text: []})
       return acc
+    } else {
+      let emacs = [].push({})
+      console.log(emacs)
     }
   }
 
@@ -43,8 +57,8 @@ const reducer = (acc, val, idx, arr) => {
 
   if (startmatch) {
     const emphasis = identifyEmphasis(startmatch[1].charAt(0))
-    if (emphasis && emphasis.endMatch.test(arr.slice(idx).join(' '))) {
-      acc.push({ type: emphasis.type, text: [] })
+    if (emphasis && emphasis.endMatch.test(arr.slice(idx).join(" "))) {
+      acc.push({type: emphasis.type, text: []})
     }
   }
 
@@ -52,21 +66,19 @@ const reducer = (acc, val, idx, arr) => {
 
   const endmatch = val.match(/\S(\*|\/|\_|\+|\>)$/)
   if (endmatch) {
-    acc.push({ type: 'text', text: [] })
+    acc.push({type: "text", text: []})
   }
   return acc
 }
 
 const tokenizeContent = text => {
-  if (
-    !/^.*(\*|\/|\_|\+|https:\/\/|http:\/\/|\<\d\d\d\d\-\d\d\-\d\d).*/.test(text)
-  ) {
-    return [{ type: 'text', text: text }]
+  if (!eligibile.test(text)) {
+    return [{type: "text", text: text}]
   }
   return text
-    .split(' ')
-    .reduce(reducer, [{ type: 'text', text: [] }])
-    .map(line => ({ type: line.type, text: line.text.join(' ') }))
+    .split(" ")
+    .reduce(reducer, [{type: "text", text: []}])
+    .map(line => ({type: line.type, text: line.text.join(" ")}))
 }
 
 module.exports = tokenizeContent
