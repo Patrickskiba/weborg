92ebde6143ee5d63706ef3c7e3b18a368defdd09
Patrick Skiba <pskiba@posteo.net>
2019-10-30 06:00:09 -0400

    starting new file explorer menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { useContext, useState } from 'react'
-import { AddMode, EditMode } from './AddEditMode'
+import { AddMode, EditMode } from './AddEditMode/index'
 import FileExplorer from './FileExplorer'
 import MoveNode from './MoveNode'
 import RenderOrgNodes from './RenderOrgNodes'
 import { StoreContext } from './Store'
-import BottomBar from './BottomBar'
+import BottomBar from './BottomBar/index'
 
 const MainArea = ({ shouldSubmit }) => {
   const { mode } = useContext(StoreContext)
@@ -21,11 +21,15 @@ const MainArea = ({ shouldSubmit }) => {
 export default () => {
   const [sideBarVisible, setSideBarVisible] = useState(false)
   const [shouldSubmit, setShouldSubmit] = useState()
+  const [fileExplorer, setFileExplorer] = useState(false)
 
   return (
     <>
       <FileExplorer sideBarVisible={sideBarVisible} setSideBarVisible={setSideBarVisible} />
       <MainArea shouldSubmit={shouldSubmit} />
+      {fileExplorer && (<div className='file-explorer-container'>
+                          <div className='file-explorer-list'></div>
+                        </div>)}
       <BottomBar
         sideBarVisible={sideBarVisible}
         setSideBarVisible={setSideBarVisible}
