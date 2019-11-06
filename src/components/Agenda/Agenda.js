6e7b45dcebd4c51807bed5f98af28798a8e77a92
Patrick Skiba <pskiba@posteo.net>
2019-11-06 05:54:26 -0500

    More agenda mode additions

--- src/components/Agenda/Agenda.js
+++ src/components/Agenda/Agenda.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getAgendaWeekView } from '../../parser/agenda'
+import format from 'date-fns/format'
 
 export default ({ showAgenda, setShowAgenda }) => {
   const [agendaList, setAgendaList] = useState([])
@@ -13,9 +14,18 @@ export default ({ showAgenda, setShowAgenda }) => {
       {showAgenda && (
         <>
           <div className='file-explorer-darken' onClick={() => setShowAgenda(false)} />
-          <div className={`file-explorer-container tall`}>
-            <div className={`file-explorer-list tall`}>
-              <div className='file-explorer-list-container'>hi</div>
+          <div className='file-explorer-container tall'>
+            <div className='file-explorer-list tall'>
+              <div className='file-explorer-list-container'>
+                {agendaList.map(agenda => (
+                  <div className='agenda-row'>
+                    <div>{format(agenda.day, 'cccc')}</div>
+                    <div>{format(agenda.day, 'd')}</div>
+                    <div>{format(agenda.day, 'LLLL')}</div>
+                    <div>{format(agenda.day, 'yyyy')}</div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </>
