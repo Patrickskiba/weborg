e361f27e528d01fec495ac1dfe4487eedfca274d
Patrick Skiba <pskiba@posteo.net>
2019-11-07 05:36:55 -0500

    More agenda stuffs

--- src/components/Agenda/Agenda.js
+++ src/components/Agenda/Agenda.js
@@ -8,6 +8,7 @@ export default ({ showAgenda, setShowAgenda }) => {
   useEffect(() => {
     getAgendaWeekView().then(agendas => setAgendaList(agendas))
   }, [])
+  console.log(agendaList)
 
   return (
     <>
@@ -17,12 +18,13 @@ export default ({ showAgenda, setShowAgenda }) => {
           <div className='file-explorer-container tall'>
             <div className='file-explorer-list tall'>
               <div className='file-explorer-list-container'>
-                {agendaList.map(agenda => (
-                  <div className='agenda-row'>
-                    <div>{format(agenda.day, 'cccc')}</div>
+                {agendaList.map((agenda, idx) => (
+                  <div key={`agenda-${idx}`} className='agenda-row'>
+                    <div className='agenda-weekday'>{format(agenda.day, 'cccc')}</div>
                     <div>{format(agenda.day, 'd')}</div>
                     <div>{format(agenda.day, 'LLLL')}</div>
                     <div>{format(agenda.day, 'yyyy')}</div>
+                    <div className='agenda-blank'>{idx === 0 ? 'W40' : ''}</div>
                   </div>
                 ))}
               </div>
