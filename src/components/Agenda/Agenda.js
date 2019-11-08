66b15a64be5fb55b47990d3d4a250c1a0dc553dd
Patrick Skiba <pskiba@posteo.net>
2019-11-08 05:55:59 -0500

    more agenda fields being populated

--- src/components/Agenda/Agenda.js
+++ src/components/Agenda/Agenda.js
@@ -16,16 +16,28 @@ export default ({ showAgenda, setShowAgenda }) => {
         <>
           <div className='file-explorer-darken' onClick={() => setShowAgenda(false)} />
           <div className='file-explorer-container tall'>
+            <div className='agenda-header'>W40</div>
             <div className='file-explorer-list tall'>
               <div className='file-explorer-list-container'>
                 {agendaList.map((agenda, idx) => (
-                  <div key={`agenda-${idx}`} className='agenda-row'>
-                    <div className='agenda-weekday'>{format(agenda.day, 'cccc')}</div>
-                    <div>{format(agenda.day, 'd')}</div>
-                    <div>{format(agenda.day, 'LLLL')}</div>
-                    <div>{format(agenda.day, 'yyyy')}</div>
-                    <div className='agenda-blank'>{idx === 0 ? 'W40' : ''}</div>
-                  </div>
+                  <>
+                    <div key={`agenda-${idx}`} className='agenda-row'>
+                      <div className='agenda-weekday'>{format(agenda.day, 'cccc')}</div>
+                      <div>{format(agenda.day, 'd')}</div>
+                      <div>{format(agenda.day, 'LLLL')}</div>
+                      <div>{format(agenda.day, 'yyyy')}</div>
+                    </div>
+                    {agenda.tasks.length !== 0 && (
+                      <>
+                        {agenda.tasks.map(t => (
+                          <>
+                            <div>{t.file.replace('.org', ':')}</div>
+                            <div>{t.headline.replace('* ', '').replace('*', '')}</div>
+                          </>
+                        ))}
+                      </>
+                    )}
+                  </>
                 ))}
               </div>
             </div>
