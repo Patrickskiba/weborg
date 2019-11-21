7c130d67254850f316c31d0cc3eba40dd2286861
Patrick Skiba <pskiba@posteo.net>
2019-11-21 05:59:16 -0500

    more repeater work, calculating tasks within a specificed range of time

--- src/parser/agenda.js
+++ src/parser/agenda.js
@@ -4,10 +4,15 @@ import startOfWeek from 'date-fns/startOfWeek'
 import startOfMonth from 'date-fns/startOfMonth'
 import getDaysInMonth from 'date-fns/getDaysInMonth'
 import addDays from 'date-fns/addDays'
+import addWeeks from 'date-fns/addWeeks'
 import getDayOfYear from 'date-fns/getDayOfYear'
 import isToday from 'date-fns/isToday'
 import isPast from 'date-fns/isPast'
 import differenceInCalendarDays from 'date-fns/differenceInCalendarDays'
+import differenceInHours from 'date-fns/differenceInHours'
+import differenceInDays from 'date-fns/differenceInDays'
+import differenceInWeeks from 'date-fns/differenceInWeeks'
+import differenceInMonths from 'date-fns/differenceInMonths'
 
 const isRepeater = task => task.match(/(\+|\+\+|\.\+)(\d+)(y|w|m|d|h)/)
 
@@ -36,6 +41,34 @@ export const getDaysOfMonth = (date = new Date()) => {
 
 const taskRegExp = /((SCHEDULED|DEADLINE):\s*<\d\d\d\d-\d\d-\d\d\s*(\w\w\w\s*)?(\d\d:\d\d:(AM|PM|am|pm)\s*)?((\+|\+\+|\.\+)\d+(y|w|m|d|h))?>)/g
 const headlineRegExp = /^(\*+)\s+(?:(TODO|DONE)\s+)?(?:\[#(A|B|C)\]\s+)?(.*?)\s*(:(?:\w+:)+)?$/
+
+const populateRepeatTasks = (repeaterList, days) => {
+  console.log(repeaterList)
+  console.log(days)
+
+  const unit = repeaterList[0].repeater.repeaterUnit
+
+  console.log(repeaterList[0].date)
+  console.log(days[0])
+
+  if (unit === 'h') {
+    console.log(differenceInHours(days[0], repeaterList[0].date))
+  }
+
+  if (unit === 'd') {
+    console.log(differenceInDays(days[0], repeaterList[0].date))
+  }
+
+  if (unit === 'w') {
+    const diff = differenceInWeeks(days[0], repeaterList[0].date)
+    const result = addWeeks(repeaterList[0].date, diff)
+    console.log(result)
+  }
+
+  if (unit === 'm') {
+    console.log(differenceInMonths(days[0], repeaterList[0].date))
+  }
+}
 
 const getAgenda = (text, file) => {
   const lines = text.split('\n')
@@ -81,13 +114,15 @@ const agenda = async () => {
 
   const sortedAgenda = (await agendas).sort((curr, next) => next - curr)
 
-  const repeaterList = sortedAgenda.filter(task => task.repeater)
-
   return sortedAgenda
 }
 
 const getAgendaForRange = async days => {
   const agendaList = await agenda()
+
+  const repeaterList = agendaList.filter(task => task.repeater)
+
+  const repeats = populateRepeatTasks(repeaterList, days)
 
   return days.map(day => {
     const tasks = []
